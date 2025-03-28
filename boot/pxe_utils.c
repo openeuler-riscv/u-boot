3b7fdb396a1aec6a49b210f80e46e028cc3a216e
Hangfan Li <lihangfan@iscas.ac.cn>
2025-03-28 17:02:52 +0800

    fix pxe load

--- boot/pxe_utils.c
+++ boot/pxe_utils.c
@@ -104,7 +104,7 @@ static int get_relfile(struct pxe_context *ctx, const char *file_path,
 	size_t path_len;
 	char relfile[MAX_TFTP_PATH_LEN + 1];
 	char addr_buf[18];
-	ulong size;
+	ulong size=0x10000000;
 	int ret;
 
 	if (file_path[0] == '/' && ctx->allow_abs_path)
