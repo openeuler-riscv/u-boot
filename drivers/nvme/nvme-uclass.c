beb71222e041a4333c53d96613e17ddcb1be8433
Hangfan Li <lihangfan@iscas.ac.cn>
2025-04-18 12:28:20 +0800

    support bootstd from nvme

--- drivers/nvme/nvme-uclass.c
+++ drivers/nvme/nvme-uclass.c
@@ -8,8 +8,21 @@
 
 #include <common.h>
 #include <dm.h>
+#include <bootdev.h>
+
+static int nvme_post_bind(struct udevice *dev)
+{
+	int ret;
+
+	ret = bootdev_setup_for_dev(dev, "nvme_bootdev");
+	if (ret)
+		return log_msg_ret("bootdev", ret);
+
+	return 0;
+}
 
 UCLASS_DRIVER(nvme) = {
 	.name	= "nvme",
 	.id	= UCLASS_NVME,
+	.post_bind	= nvme_post_bind,
 };
