e4f83c80acb0ccca47fc7820cef2b9227301f4e3
Hangfan Li <lihangfan@iscas.ac.cn>
2025-10-20 15:22:17 +0800

    Finetune HiFive Premier P550 configuration

--- include/configs/hifive_premier_p550.h
+++ include/configs/hifive_premier_p550.h
@@ -26,10 +26,7 @@
 
 #define CFG_MALLOC_F_ADDR   0xf0000000
 
-#include <config_distro_bootcmd.h>
-
 #define CFG_EXTRA_ENV_SETTINGS \
-    "bootdelay=2\0" \
     "fdt_high=0xffffffffffffffff\0" \
     "initrd_high=0xffffffffffffffff\0" \
     "kernel_addr_r=0x84000000\0" \
@@ -45,22 +42,6 @@
     "kernel_comp_size=0x4000000\0" \
     "emmc_dev=0\0" \
     "splashimage=0xe0000000\0" \
-    "splashpos=1660,0\0" \
-    "usbupdate=ext4load usb 0 0x90000000 usbupdate.scr;source 0x90000000\0" \
-    "sdupdate=ext4load mmc 1:1 0x90000000 sdupdate.scr;source 0x90000000\0" \
-    "typeid_efi=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\0" \
-    "typeid_swap=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F\0" \
-    "typeid_filesystem=0FC63DAF-8483-4772-8E79-3D69D8477DE4\0" \
-    "uuid_boot=44b7cb94-f58c-4ba6-bfa4-7d2dce09a3a5\0" \
-    "uuid_root=b0f77ad6-36cd-4a99-a8c0-31d73649aa08\0" \
-    "uuid_swap=5ebcaaf0-e098-43b9-beef-1f8deedd135e\0" \
-    "partitions=name=boot,start=1MiB,size=512MiB,type=${typeid_efi},uuid=${uuid_boot};name=swap,size=4096MiB,type=${typeid_swap},uuid=${uuid_swap};name=root,size=30GiB,type=${typeid_filesystem},uuid=${uuid_root};name=userdata,type=${typeid_filesystem},size=-;\0" \
-    "gpt_partition=gpt write mmc ${emmc_dev} $partitions\0" \
-    "boot_targets=mmc1 usb ahci mmc0\0"
-
-#undef CONFIG_BOOTCOMMAND
-#define CONFIG_BOOTCOMMAND \
-    "bootflow scan -b;"
-
+    "splashpos=1660,0\0"
 
 #endif /* __CONFIG_H */
